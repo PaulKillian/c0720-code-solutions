f227f4483788936ae35580d304c153f4dd99bce4
PaulKillian <psk65lava@gmail.com>
2020-09-01 11:54:15 -0700

    update

--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-unused-vars */
 function isUpperCased(word) {
-  let letterCheck = '';
-  for (let i = 0; i < word.length; i++) {
-    letterCheck += word[i].toUpperCase();
-  }
-  if (letterCheck[0] === word[0] && letterCheck[1] === word[1]) {
-    return true;
-  } if (letterCheck[0] !== word[0]) {
-    return false;
-  } else if (letterCheck[0] === word[0] && letterCheck[1] !== word[1]) {
+  const letterCheck = word.toUpperCase();
+  if (word === letterCheck) return true;
+  else {
     return false;
   }
 }
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,13 +1,23 @@
 /* eslint-disable no-unused-vars */
 function isVowel(char) {
   switch (char) {
+    case 'A':
+      return true;
     case 'a':
       return true;
     case 'E':
       return true;
+    case 'e':
+      return true;
     case 'I':
       return true;
+    case 'i':
+      return true;
+    case 'O':
+      return true;
     case 'o':
+      return true;
+    case 'U':
       return true;
     case 'u':
       return true;
