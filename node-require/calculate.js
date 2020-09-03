cdb501812a62949c6026029ddb8d07ed77b66007
PaulKillian <psk65lava@gmail.com>
2020-09-03 10:02:52 -0700

    add /* eslint-disable no-console */

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -12,17 +12,17 @@ const integer1 = parseInt(process.argv[2]);
 const integer2 = parseInt(process.argv[4]);
 
 if (operator[0] === 'plus') {
-  add(integer1, integer2);
+  console.log('Result:', add(integer1, integer2));
 }
 
 if (operator[0] === 'minus') {
-  subtract(integer1, integer2);
+  console.log('Result:', subtract(integer1, integer2));
 }
 
 if (operator[0] === 'times') {
-  multiply(integer1, integer2);
+  console.log('Result:', multiply(integer1, integer2));
 }
 
 if (operator[0] === 'over') {
-  divide(integer1, integer2);
+  console.log('Result:', divide(integer1, integer2));
 }
