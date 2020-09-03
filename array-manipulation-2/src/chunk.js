60cec4b8d34bfd5a45dff1a8ec1eb0a2d5b08d44
PaulKillian <psk65lava@gmail.com>
2020-09-03 09:57:06 -0700

    chunk update

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,25 +1,15 @@
 /* eslint-disable no-unused-vars */
 function chunk(array, size) {
-  // const result = [];
-  // const result2 = [];
-  // const finalResult = [];
-  // for (let i = 0; i < array.length - size; i++) {
-  //   result.push(array[i]);
-  // } for (let x = size; x < array.length; x++) {
-  //   result2.push(array[x]);
-  // }
-  // finalResult.push(result, result2);
-  // return finalResult;
   const result = [];
   const result2 = [];
   const finalResult = [];
   for (let i = 0; i < size; i++) {
-    // if (array[i] === undefined) {
-    //   return result;
-    // }
-    result.push(array[i]);
+    for (let x = 0; x < result2.length; x++) {
+      result2[x] = array[i];
+    }
   }
-  for (let x = size; x < array.length; x++) { result2.push(array[x]); }
-  finalResult.push(result, result2);
+
+  // for (let x = size; x < array.length; x++) { result2.push(array[x]); }
+  finalResult.push(result);
   return finalResult;
 }
