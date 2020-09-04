bc41b69f93451d1d2f68a77bda1868c0c3b105c2
PaulKillian <psk65lava@gmail.com>
2020-09-04 16:29:33 -0700

    Completed User can add a note

--- node-notes-cli/add-note.js
+++ node-notes-cli/add-note.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-console */
 const fs = require('fs');
+const jsonNotes = require('./data.json');
+jsonNotes.notes[jsonNotes.nextID] = process.argv[2];
+jsonNotes.nextID++;
 
-fs.writeFile('./' + process.argv[2], err => {
+fs.writeFile('data.json', JSON.stringify(jsonNotes, null, 2), (err, data) => {
   if (err) throw err;
 });
