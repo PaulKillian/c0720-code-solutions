085402c506978685fe7f8a5f8c7f2900658068e0
PaulKillian <psk65lava@gmail.com>
2020-09-04 17:55:14 -0700

    Completed delete and update note

--- node-notes-cli/delete-note.js
+++ node-notes-cli/delete-note.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-console */
+const fs = require('fs');
+const jsonNotes = require('./data.json');
+delete jsonNotes.notes[process.argv[2]];
+
+fs.writeFile('data.json', JSON.stringify(jsonNotes, null, 2), (err, data) => {
+  if (err) throw err;
+});
--- node-notes-cli/update-note.js
+++ node-notes-cli/update-note.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-console */
+const fs = require('fs');
+const jsonNotes = require('./data.json');
+jsonNotes.notes[process.argv[2]] = process.argv[3];
+
+fs.writeFile('data.json', JSON.stringify(jsonNotes, null, 2), (err, data) => {
+  if (err) throw err;
+});
