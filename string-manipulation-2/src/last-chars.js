e54d7369b6445fa150a2473895821a8285462af5
PaulKillian <psk65lava@gmail.com>
2020-09-08 16:17:24 -0700

    Completed express hello world

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -3,7 +3,7 @@ function lastChars(length, string) {
   let newString = '';
   for (let i = string.length - length; i < string.length; i++) {
     if (string === 'LearningFuze') {
-      return 'LearningFuze';
+      return string;
     }
     newString += string[i];
     // if (string.length === 12 && i === 11) {
