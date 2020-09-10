878afb967fd3847d0447b9da8c1070a55d0c4913
PaulKillian <psk65lava@gmail.com>
2020-09-09 17:05:18 -0700

    Completed Express POST JSON

---
+++ express-post-json/index.js
@@ -0,0 +1,24 @@
+const grades = [];
+let nextId = 1;
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
+});
+
+app.post('/api/grades', (req, res) => {
+  res.status(201);
+  req.body['id:'] = `${nextId}`;
+  grades.push(req.body);
+  nextId++;
+  res.json(grades);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening...');
+});
