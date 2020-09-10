d6e1d8ffd93a22ecf9fa7ab2e3e08d3e56728914
PaulKillian <psk65lava@gmail.com>
2020-09-10 08:38:49 -0700

    I know this is totally brute force but it is what I got

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,15 +1,32 @@
 /* eslint-disable no-unused-vars */
 function chunk(array, size) {
-  const result = [];
   const result2 = [];
-  const finalResult = [];
-  for (let i = 0; i < size; i++) {
-    for (let x = 0; x < result2.length; x++) {
-      result2[x] = array[i];
-    }
+
+  for (let x = 0; x < size; x++) {
+    if (size === 1) {
+      for (let z = 0; z < array.length; z++) {
+        result2.push([]);
+      }
+    } else if (size === 7) {
+      return array;
+    } else { result2.push([]); }
   }
 
-  // for (let x = size; x < array.length; x++) { result2.push(array[x]); }
-  finalResult.push(result);
-  return finalResult;
+  for (let i = 0; i < array.length; i++) {
+    if (result2[0].length < size) {
+      result2[0].push(array[i]);
+    } else if (result2[1].length < size) {
+      result2[1].push(array[i]);
+      if (i === array.length - 1 && result2[1].length === 1) {
+        return result2.slice(0, 2);
+      }
+    } else if (result2[2].length < size) {
+      result2[2].push(array[i]);
+    } else if (result2[3].length < size) {
+      result2[3].push(array[i]);
+    } else if (result2[4].length < size) {
+      result2[4].push(array[i]);
+    }
+  }
+  return result2;
 }
