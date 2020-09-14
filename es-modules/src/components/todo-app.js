e1aebf1e1886a27224f654a224586b460adf5f2a
PaulKillian <psk65lava@gmail.com>
2020-09-14 14:43:31 -0700

    added relative path

--- es-modules/src/components/todo-app.js
+++ es-modules/src/components/todo-app.js
@@ -1,6 +1,6 @@
 import { createElement, noop } from '../lib';
-import TodoList from 'todo-list';
-import TodoForm from 'todo-form';
+import TodoList from './todo-list';
+import TodoForm from './todo-form';
 
 class TodoApp {
   constructor(container, nextId, todos, onUpdate = noop) {
