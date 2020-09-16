c88aaff0e0d842f4e2602c2eaf2d320d81440ed4
PaulKillian <psk65lava@gmail.com>
2020-09-16 05:08:28 -0700

    almost done

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+const defaults = (target, source) => {
+  const newTarget = target;
+  for (const skey in source) {
+    if (!target.skey === true) {
+      newTarget[skey] = source[skey];
+    }
+  }
+  return newTarget;
+};
