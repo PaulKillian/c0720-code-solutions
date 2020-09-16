c3a5b78bf5db70d4b0636bd2dce25941fab0d812
PaulKillian <psk65lava@gmail.com>
2020-09-15 19:39:56 -0700

    3 object maniulation problems finished

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-unused-vars */
+function invert(source) {
+  const picks = {};
+  for (const foo in source) {
+    picks[source[foo]] = foo;
+  }
+  return picks;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function pick(source, keys) {
+  const picks = {};
+  for (let i = 0; i < keys.length; i++) {
+    for (const foo in source) {
+      if (source[foo] !== undefined && foo === keys[i]) {
+        picks[foo] = source[foo];
+      }
+    }
+  }
+  return picks;
+}
