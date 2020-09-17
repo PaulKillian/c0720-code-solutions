5b5fb3cbd3f075ba5dcb7c5547ffdc3ab0657e06
PaulKillian <psk65lava@gmail.com>
2020-09-16 19:28:38 -0700

    setInterval update

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -1,37 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HotButton extends React.Component {
+class StopWatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isClicked: false };
+    this.state = {
+      seconds: 0,
+      status: false
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.timer = this.timer.bind(this);
+    this.clock = this.clock.bind(this);
   }
 
   handleClick() {
-    if (this.state.isClicked === false) {
-      this.setState({ isClicked: true });
+    const watch = setInterval(this.timer, 1000);
+    if (this.state.status === true) {
+      clearInterval(watch);
+    }
+    this.setState({ status: true });
+  }
+
+  timer() {
+    this.setState({ seconds: this.state.seconds + 1 });
+    this.clock();
+  }
+
+  clock() {
+    if (this.state.status === false) {
+      return (
+        <div className="container flex">
+          <div className="watch flex">
+            <div className="number-dimensions">{this.state.seconds}</div>
+          </div>
+          <div className="icon-dimensions flex-bottom">
+            <i className="far fa-play-circle flex-bottom " onClick={this.handleClick}></i>
+          </div>
+        </div>
+      );
     } else {
-      this.setState({ isClicked: false });
+      return (
+        <div className="container flex">
+          <div className="watch flex">
+            <div className="number-dimensions">{this.state.seconds}</div>
+          </div>
+          <div className="icon-dimensions flex-bottom">
+            <i className="far fa-pause-circle flex-bottom " onClick={this.handleClick}></i>
+          </div>
+        </div>
+      );
     }
   }
 
   render() {
-
-    return (
-      <div className="container flex">
-        <div className="watch flex" onClick={this.handleClick}>
-          <div className="number-dimensions">0</div>
-        </div>
-        <div className="icon-dimensions flex-bottom">
-          <i className="far fa-play-circle flex-bottom " onClick={this.handleClick}></i>
-        </div>
-      </div>
-    );
+    return this.clock();
   }
 }
 
 ReactDOM.render(
-  <HotButton />,
+  <StopWatch />,
   document.getElementById('root')
 );
