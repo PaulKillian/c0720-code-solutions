ddc5327f9d241ab80793c61610ec2deed56b4135
PaulKillian <psk65lava@gmail.com>
2020-09-17 11:00:33 -0700

    Completed React Form Controls

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -1,1 +1,38 @@
 /* eslint-disable no-console */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form action="" method="post" onSubmit={this.handleSubmit}>
+        <label> Email:
+          <input type="email" name="user_email" onChange={this.handleChange}></input>
+        </label>
+        <button>Sign Up</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.getElementById('root')
+);
