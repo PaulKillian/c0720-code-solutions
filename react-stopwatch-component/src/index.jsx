ca5e560b02206a269eb2c6f6ffe8ffca659d1008
PaulKillian <psk65lava@gmail.com>
2020-09-17 05:56:18 -0700

    Completed React-Stopwatch Component

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -8,52 +8,63 @@ class StopWatch extends React.Component {
       seconds: 0,
       status: false
     };
-    this.handleClick = this.handleClick.bind(this);
-    this.timer = this.timer.bind(this);
-    this.clock = this.clock.bind(this);
+    this.handleStart = this.handleStart.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+    this.initialStart = this.initialStart.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
-  handleClick() {
-    const watch = setInterval(this.timer, 1000);
-    if (this.state.status === true) {
-      clearInterval(watch);
-    }
-    this.setState({ status: true });
-  }
-
-  timer() {
+  initialStart() {
     this.setState({ seconds: this.state.seconds + 1 });
-    this.clock();
+    this.render();
   }
 
-  clock() {
+  handleStart() {
+    this.start = setInterval(this.initialStart, 1000);
+    if (this.state.status === false) {
+      this.setState({ status: true });
+    } else {
+      this.setState({ status: false });
+      this.handleStop();
+    }
+  }
+
+  handleStop() {
+    if (this.state.status === true) {
+      clearInterval(this.start);
+    }
+    this.setState({ status: false });
+  }
+
+  handleReset() {
+    this.setState({ seconds: 0 });
+    this.handleStop();
+  }
+
+  render() {
     if (this.state.status === false) {
       return (
         <div className="container flex">
-          <div className="watch flex">
+          <div className="watch flex cursor" onClick={this.handleReset}>
             <div className="number-dimensions">{this.state.seconds}</div>
           </div>
-          <div className="icon-dimensions flex-bottom">
-            <i className="far fa-play-circle flex-bottom " onClick={this.handleClick}></i>
+          <div className="icon-dimensions flex-bottom cursor">
+            <i className="far fa-play-circle flex-bottom" onClick={this.handleStart}></i>
           </div>
         </div>
       );
     } else {
       return (
         <div className="container flex">
-          <div className="watch flex">
+          <div className="watch flex cursor" onClick={this.handleReset}>
             <div className="number-dimensions">{this.state.seconds}</div>
           </div>
-          <div className="icon-dimensions flex-bottom">
-            <i className="far fa-pause-circle flex-bottom " onClick={this.handleClick}></i>
+          <div className="icon-dimensions flex-bottom cursor">
+            <i className="far fa-pause-circle flex-bottom" onClick={this.handleStop}></i>
           </div>
         </div>
       );
     }
-  }
-
-  render() {
-    return this.clock();
   }
 }
 
