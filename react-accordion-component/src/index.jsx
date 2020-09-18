d99ea5e964f1da823a72235213374d5f1438348d
PaulKillian <psk65lava@gmail.com>
2020-09-18 13:39:19 -0700

    import update

--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -1,25 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class Accordion extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isTopic: 0 };
-  }
-
-  render() {
-    return (
-      <div className="flex">
-        <div>Hypertext Markup Language</div>
-        <div className="hidden">{this.state.isTopic.html}</div>
-        <div>Cascading Style Sheet</div>
-        <div className="hidden"></div>
-        <div>JavaScript</div>
-        <div className="hidden"></div>
-      </div>
-    );
-  }
-}
+import Accordion from './index.jsx';
 
 ReactDOM.render(
   <Accordion />,
